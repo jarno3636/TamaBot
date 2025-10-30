3538c8ff37d2228104078f5e3c3876634108c941
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-10-30 13:31:58 -0500

    Update ipfs.ts

--- lib/ipfs.ts
+++ lib/ipfs.ts
@@ -1,4 +1,6 @@
+// lib/ipfs.ts
+const GATEWAY = process.env.NEXT_PUBLIC_FILEBASE_GATEWAY_URL || "https://ipfs.filebase.io";
 export function ipfsToHttp(uri?: string) {
   if (!uri) return "";
-  return uri.replace("ipfs://", "https://cloudflare-ipfs.com/ipfs/");
+  return uri.replace(/^ipfs:\/\//, `${GATEWAY.replace(/\/$/, "")}/ipfs/`);
 }
