0518c9e858a7b210b4462531e5148cae222d3555
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-11-08 09:37:19 -0600

    Update abi.ts

--- lib/abi.ts
+++ lib/abi.ts
@@ -1,41 +1,181 @@
-export const ATTESTATION_VERIFIER = {
-  address: "0x913f3f178038e99938a38718014f1e0b41197a2b" as const,
+// Basebots (on-chain SVG robots)
+// Network: Base
+export const BASEBOTS = {
+  address: "0xe36cf57cf3684c73bed170072f086f68ff3e6cfe" as const,
   abi: [
-    {"inputs":[{"internalType":"address","name":"_owner","type":"address"},{"internalType":"address","name":"_signer","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},
-    {"inputs":[],"name":"ECDSAInvalidSignature","type":"error"},
-    {"inputs":[{"internalType":"uint256","name":"length","type":"uint256"}],"name":"ECDSAInvalidSignatureLength","type":"error"},
-    {"inputs":[{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"ECDSAInvalidSignatureS","type":"error"},
-    {"inputs":[],"name":"InvalidShortString","type":"error"},
-    {"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"OwnableInvalidOwner","type":"error"},
-    {"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"OwnableUnauthorizedAccount","type":"error"},
-    {"inputs":[{"internalType":"string","name":"str","type":"string"}],"name":"StringTooLong","type":"error"},
-    {"anonymous":false,"inputs":[],"name":"EIP712DomainChanged","type":"event"},
-    {"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},
-    {"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"signer","type":"address"}],"name":"SignerUpdated","type":"event"},
-    {"inputs":[],"name":"SCORE_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},
-    {"inputs":[],"name":"eip712Domain","outputs":[{"internalType":"bytes1","name":"fields","type":"bytes1"},{"internalType":"string","name":"name","type":"string"},{"internalType":"string","name":"version","type":"string"},{"internalType":"uint256","name":"chainId","type":"uint256"},{"internalType":"address","name":"verifyingContract","type":"address"},{"internalType":"bytes32","name":"salt","type":"bytes32"},{"internalType":"uint256[]","name":"extensions","type":"uint256[]"}],"stateMutability":"view","type":"function"},
-    {"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},
-    {"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},
-    {"inputs":[{"internalType":"address","name":"_signer","type":"address"}],"name":"setSigner","outputs":[],"stateMutability":"nonpayable","type":"function"},
-    {"inputs":[],"name":"signer","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},
-    {"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},
-    {"inputs":[{"internalType":"bytes","name":"payload","type":"bytes"},{"internalType":"bytes","name":"sig","type":"bytes"}],"name":"verifyAndScore","outputs":[{"internalType":"uint256","name":"fid","type":"uint256"},{"internalType":"uint256","name":"day","type":"uint256"},{"internalType":"uint64","name":"dss","type":"uint64"},{"internalType":"bool","name":"valid","type":"bool"}],"stateMutability":"view","type":"function"}
-  ] as const
-};
+    // -------- Constructor --------
+    {
+      "inputs": [
+        { "internalType": "address", "name": "initialOwner", "type": "address" },
+        { "internalType": "address", "name": "royaltyReceiver", "type": "address" },
+        { "internalType": "uint96",  "name": "royaltyBps",     "type": "uint96" }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
 
-export const TAMABOT_CORE = {
-  address: "0x25f68f020e0c8db4b1b7e2d89477c324944a7d51" as const,
-  abi: [
-    {"inputs":[{"internalType":"address","name":"_royaltyReceiver","type":"address"},{"internalType":"uint96","name":"_royaltyBps","type":"uint96"},{"internalType":"string","name":"_baseRenderer","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},
-    {"inputs":[],"name":"MAX_SUPPLY","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
-    {"inputs":[{"internalType":"uint64","name":"fid","type":"uint64"}],"name":"mint","outputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"stateMutability":"payable","type":"function"},
-    {"inputs":[],"name":"mintFee","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
-    {"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},
-    {"inputs":[{"internalType":"uint256","name":"fid","type":"uint256"}],"name":"tokenIdByFID","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
-    {"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"feed","outputs":[],"stateMutability":"nonpayable","type":"function"},
-    {"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"play","outputs":[],"stateMutability":"nonpayable","type":"function"},
-    {"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"clean","outputs":[],"stateMutability":"nonpayable","type":"function"},
-    {"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"rest","outputs":[],"stateMutability":"nonpayable","type":"function"},
-    {"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getState","outputs":[{"components":[{"internalType":"uint64","name":"level","type":"uint64"},{"internalType":"uint64","name":"xp","type":"uint64"},{"internalType":"int32","name":"mood","type":"int32"},{"internalType":"uint64","name":"hunger","type":"uint64"},{"internalType":"uint64","name":"energy","type":"uint64"},{"internalType":"uint64","name":"cleanliness","type":"uint64"},{"internalType":"uint64","name":"lastTick","type":"uint64"},{"internalType":"uint64","name":"fid","type":"uint64"}],"internalType":"struct PetState","name":"","type":"tuple"}],"stateMutability":"view","type":"function"}
+    // -------- Custom Errors --------
+    { "inputs": [], "name": "AlreadyMinted",   "type": "error" },
+    { "inputs": [], "name": "BadSig",          "type": "error" },
+    { "inputs": [], "name": "ConfigFrozen",    "type": "error" },
+    { "inputs": [], "name": "DirectETH",       "type": "error" },
+    { "inputs": [], "name": "FIDRequired",     "type": "error" },
+    { "inputs": [], "name": "IncorrectPrice",  "type": "error" },
+    { "inputs": [], "name": "MaxSupply",       "type": "error" },
+    { "inputs": [], "name": "Nonexistent",     "type": "error" },
+    { "inputs": [], "name": "SigExpired",      "type": "error" },
+    { "inputs": [], "name": "UseMintWithSig",  "type": "error" },
+    { "inputs": [], "name": "VerifierUnset",   "type": "error" },
+
+    // -------- Events --------
+    { "anonymous": false, "inputs": [], "name": "EIP712DomainChanged", "type": "event" },
+    {
+      "anonymous": false,
+      "inputs": [
+        { "indexed": true,  "internalType": "address", "name": "minter", "type": "address" },
+        { "indexed": true,  "internalType": "uint256", "name": "fid",    "type": "uint256" }
+      ],
+      "name": "Minted", "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [{ "indexed": false, "internalType": "uint256", "name": "price", "type": "uint256" }],
+      "name": "MintPriceSet", "type": "event"
+    },
+    { "anonymous": false, "inputs": [], "name": "ConfigFrozenSet", "type": "event" },
+    {
+      "anonymous": false,
+      "inputs": [{ "indexed": false, "internalType": "bool", "name": "paused", "type": "bool" }],
+      "name": "PausedSet", "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        { "indexed": true,  "internalType": "address", "name": "receiver", "type": "address" },
+        { "indexed": false, "internalType": "uint96",  "name": "bps",      "type": "uint96" }
+      ],
+      "name": "RoyaltySet", "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [{ "indexed": false, "internalType": "string", "name": "desc", "type": "string" }],
+      "name": "CollectionDescriptionSet", "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [{ "indexed": false, "internalType": "string", "name": "uri", "type": "string" }],
+      "name": "CollectionURISet", "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [{ "indexed": true, "internalType": "address", "name": "verifier", "type": "address" }],
+      "name": "FidVerifierSet", "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [{ "indexed": false, "internalType": "bool", "name": "enabled", "type": "bool" }],
+      "name": "FidGatingSet", "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        { "indexed": true,  "internalType": "address", "name": "previousOwner", "type": "address" },
+        { "indexed": true,  "internalType": "address", "name": "newOwner",      "type": "address" }
+      ],
+      "name": "OwnershipTransferred", "type": "event"
+    },
+
+    // -------- Constants / Views --------
+    { "inputs": [], "name": "MAX_SUPPLY", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [], "name": "mintPrice",  "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [], "name": "configFrozen", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [], "name": "totalMinted",  "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [], "name": "collectionDescription", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [], "name": "collectionURI", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [], "name": "fidVerifier",   "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [], "name": "fidGatingEnabled", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [{ "internalType": "address", "name": "", "type": "address" }], "name": "hasMinted", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" },
+
+    // OZ: metadata & interface
+    { "inputs": [{ "internalType": "uint256", "name": "tokenId", "type": "uint256" }], "name": "tokenURI", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [], "name": "contractURI", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [{ "internalType": "bytes4", "name": "interfaceId", "type": "bytes4" }], "name": "supportsInterface", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" },
+
+    // EIP-712 helper view (OZ v5 exposes this)
+    {
+      "inputs": [],
+      "name": "eip712Domain",
+      "outputs": [
+        { "internalType": "bytes1",   "name": "fields",            "type": "bytes1" },
+        { "internalType": "string",   "name": "name",              "type": "string" },
+        { "internalType": "string",   "name": "version",           "type": "string" },
+        { "internalType": "uint256",  "name": "chainId",           "type": "uint256" },
+        { "internalType": "address",  "name": "verifyingContract", "type": "address" },
+        { "internalType": "bytes32",  "name": "salt",              "type": "bytes32" },
+        { "internalType": "uint256[]","name": "extensions",        "type": "uint256[]" }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+
+    // ERC-2981 royalty view
+    {
+      "inputs": [
+        { "internalType": "uint256", "name": "tokenId",  "type": "uint256" },
+        { "internalType": "uint256", "name": "salePrice","type": "uint256" }
+      ],
+      "name": "royaltyInfo",
+      "outputs": [
+        { "internalType": "address", "name": "receiver", "type": "address" },
+        { "internalType": "uint256", "name": "royaltyAmount", "type": "uint256" }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+
+    // -------- Admin --------
+    { "inputs": [{ "internalType": "uint256", "name": "newPrice", "type": "uint256" }], "name": "setMintPrice", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
+    { "inputs": [{ "internalType": "string",  "name": "desc",     "type": "string"  }], "name": "setCollectionDescription", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
+    { "inputs": [{ "internalType": "string",  "name": "uri",      "type": "string"  }], "name": "setCollectionURI", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
+    {
+      "inputs": [
+        { "internalType": "address", "name": "receiver",     "type": "address" },
+        { "internalType": "uint96",  "name": "feeNumerator", "type": "uint96"  }
+      ],
+      "name": "setDefaultRoyalty",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    { "inputs": [{ "internalType": "bool",    "name": "p", "type": "bool" }], "name": "setPaused", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
+    { "inputs": [], "name": "freezeConfig", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
+    { "inputs": [{ "internalType": "address", "name": "verifier", "type": "address" }], "name": "setFidVerifier", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
+    { "inputs": [{ "internalType": "bool",    "name": "enabled",  "type": "bool" }], "name": "setFidGatingEnabled", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
+    { "inputs": [], "name": "withdraw", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
+
+    // Ownable
+    { "inputs": [], "name": "owner", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [], "name": "renounceOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
+    { "inputs": [{ "internalType": "address", "name": "newOwner", "type": "address" }], "name": "transferOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
+
+    // -------- Minting --------
+    {
+      "inputs": [{ "internalType": "uint256", "name": "fid", "type": "uint256" }],
+      "name": "mint",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        { "internalType": "uint256", "name": "fid",      "type": "uint256" },
+        { "internalType": "uint256", "name": "deadline", "type": "uint256" },
+        { "internalType": "bytes",   "name": "sig",      "type": "bytes" }
+      ],
+      "name": "mintWithSig",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+    }
   ] as const
 };
