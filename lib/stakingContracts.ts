2646aa1fcd289d9eae8798655707a399cd671f1e
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-12-03 05:52:40 -0600

    Update stakingContracts.ts

--- lib/stakingContracts.ts
+++ lib/stakingContracts.ts
@@ -225,6 +225,13 @@ export const CONFIG_STAKING_POOL_ABI = [
   },
   {
     inputs: [],
+    name: "creator",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
     name: "rewardRate",
     outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
     stateMutability: "view",
